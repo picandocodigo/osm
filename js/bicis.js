a44efcc62fa7b35f35c1d882b6dfb212ffa5fe2e
Fernando Briano <fernando@picandocodigo.net>
2015-02-14 17:28:13 -0200

    Adds bicycle map

---
+++ js/bicis.js
@@ -0,0 +1,10 @@
+var map = L.map('map');
+
+
+var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+OpenStreetMap_Mapnik.addTo(map);
+
+// -34.9149&lng=-56.16202
+map.setView([-34.9159, -56.16202], 15);
