8ae7905a2ff434a86940fa5c3bea543cf5c9f36e
Fernando Briano <fernando@picandocodigo.net>
2015-02-19 19:25:58 -0200

    Remove duplicated JS call\nAdds world

---
+++ js/world.js
@@ -0,0 +1,36 @@
+var map = L.map('map', {worldCopyJump: true});
+// Add OSM layer
+var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+OpenStreetMap_Mapnik.addTo(map);
+map.setView([0,0], 3);
+
+function loadJSON(path){
+  var obj = new XMLHttpRequest();
+  obj.onreadystatechange = function(){
+    if (obj.readyState === XMLHttpRequest.DONE){
+      if (obj.status === 200){
+        load_locations(JSON.parse(obj.responseText));
+      } else {
+        console.error("There was an error loading locations");
+        console.error(obj);
+      }
+    }
+  };
+
+  obj.open("GET", path, true);
+  obj.send();
+}
+
+function load_locations(data){
+  console.log(data);
+  data.forEach( function(city){
+    var marker = L.marker([city.lat, city.lng]).addTo(map);
+    var html = "<strong>" + city.name + "</strong>";
+    marker.bindPopup(html);
+    console.log(city.name);
+  });
+}
+
+loadJSON('js/world.json');
